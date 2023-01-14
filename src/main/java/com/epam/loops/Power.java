a1607d420fa1cbecc59a471403b853b853089206
Mardon Khodjaev <zombiee4407@gmail.com>
2023-01-14 15:28:22 +0500

    finished loop tasks

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,7 +3,15 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
+        System.out.println(power(numberToPrint, power));
+    }
 
+    private int power(int num, int pow) {
+        if (pow == 0) return 1;
+        if (pow == 1) return num;
+
+        int result = power(num, pow / 2);
+        return pow % 2 == 0 ? result * result : result * result * num;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,15 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = cathetusLength; j > 1; j--) {
+                System.out.print(j > i + 1 ? " " : j);
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
